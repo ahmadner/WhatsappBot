4395afc9cfd30b5d8d23fcb6956c1245bc3e84cd
AHMAD <57689837+ahmadner@users.noreply.github.com>
2021-06-06 19:08:26 +0300

    Update tubeDownloader.py

--- tubeDownloader.py
+++ tubeDownloader.py
@@ -12,17 +12,20 @@ def _tubeDownloader(video_url,ex):
         t = now.strftime(r"%m%d%Y%H%M%S")
         url = video_url[1]
         youtube = pytube.YouTube(url)
-        vName = t+str(count)
-        count +=1
-        if ex == 'd':
-            video = youtube.streams.first()
-            video.download(video_folder,filename=str(vName))
-            folderLink = (f'{video_folder}/{vName}.mp4')
-            return folderLink
-        elif ex == 'a' :
-            video = youtube.streams.filter(only_audio=True).first()
-            video.download(audio_folder,filename=str(vName))
-            os.rename(f'{audio_folder}/{vName}.mp4',f'{audio_folder}/{vName}.mp3')
-            folderLink = (f'{audio_folder}/{vName}.mp3')
-            return folderLink
+        if youtube.length > 720 : # vedio over 12 min cant download.
+            return 'Error'
+        else:
+            vName = t+str(count)
+            count +=1
+            if ex == 'd':
+                video = youtube.streams.first()
+                video.download(video_folder,filename=str(vName))
+                folderLink = (f'{video_folder}/{vName}.mp4')
+                return folderLink
+            elif ex == 'a' :
+                video = youtube.streams.filter(only_audio=True).first()
+                video.download(audio_folder,filename=str(vName))
+                os.rename(f'{audio_folder}/{vName}.mp4',f'{audio_folder}/{vName}.mp3')
+                folderLink = (f'{audio_folder}/{vName}.mp3')
+                return folderLink
     except:print ('[*] Error in tubeDownloader');return 'Error'
