3c77a12ef62de13650a7613b3a5c22c27a3a5ba7
AHMAD <57689837+ahmadner@users.noreply.github.com>
2021-06-06 13:28:42 +0300

    Update proj.py

--- proj.py
+++ proj.py
@@ -1,6 +1,6 @@
 def projList():
     proj = ['[1] download audio from YouTube',
     '[2] Download Video from YouTube',
-    '[3] search in Wikipedia'
+    '[3] search in Wikipedia',
     '[4] create random password']
     return proj
