290c514b668d4d3dd2f16419e852565e0ba70f03
AHMAD <57689837+ahmadner@users.noreply.github.com>
2021-06-09 01:15:04 +0300

    Update proj.py

--- proj.py
+++ proj.py
@@ -1,6 +1,12 @@
 def projList():
-    proj = ['[1] download audio from YouTube',
-    '[2] Download Video from YouTube',
-    '[3] search in Wikipedia',
-    '[4] create random password']
+    proj = [
+    '*Whatsapp  Services*',
+    '=============================',
+    'send number of service you want :',
+    ' ',
+    '*1* . download audio from YouTube',
+    '*2* . Download Video from YouTube',
+    '*3* . search in Wikipedia',
+    '*4* . create random password',
+    '=============================']
     return proj
