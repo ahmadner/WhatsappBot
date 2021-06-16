bb41292050ca05d9f9bf9fd65ce4cba3dbd6e8f6
AHMAD <57689837+ahmadner@users.noreply.github.com>
2021-06-16 15:34:06 +0300

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#sudo apt -y update
+sudo apt -y update
 #sudo apt-get -y update --fix-missing
-#sudo apt -y upgrade 
+sudo apt -y upgrade 
 sudo apt-get -y install python3
 sudo apt-get -y install python3-pip
 sudo pip3 install selenium
@@ -14,7 +14,7 @@ sudo wget https://chromedriver.storage.googleapis.com/90.0.4430.24/chromedriver_
 sudo wget https://github.com/mozilla/geckodriver/releases/download/v0.29.1/geckodriver-v0.29.1-linux64.tar.gz -P ~/whatsappBot/temp
 sudo apt-get -y install unzip
 sudo unzip ~/whatsappBot/temp/chromedriver_linux64.zip -d ~/whatsappBot
-sudo tar -czvf ~/whatsappBot/temp/geckodriver-v0.29.1-linux64.tar.gz ~/whatsappBot
+sudo tar xf ~/whatsappBot/temp/geckodriver-v0.29.1-linux64.tar.gz -C ~/whatsappBot
 sudo chmod 777 ~/whatsappBot/chromedriver
 sudo chmod 777 ~/whatsappBot/geckodriver
 sudo rm -rf ~/whatsappBot/temp
